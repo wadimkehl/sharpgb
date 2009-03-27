a889c089c76e14f60bb9aad387ac1cad7afd985a
wadim.kehl <wadim.kehl@f0eada0c-1a61-11de-a958-c9a343b5b891>
2009-03-27 00:20:16 +0000

    First up

---
+++ trunk/sharpGB/Forms/CPUView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sharpGB.Forms
+{
+    public partial class CPUView : Form
+    {
+        public CPUView()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateView(ref CEmulator E)
+        {
+            AField.Text = E.Processor.A.ToString("X2");
+            BField.Text = E.Processor.B.ToString("X2");
+            CField.Text = E.Processor.C.ToString("X2");
+            DField.Text = E.Processor.D.ToString("X2");
+            EField.Text = E.Processor.E.ToString("X2");
+            HField.Text = E.Processor.H.ToString("X2");
+            LField.Text = E.Processor.L.ToString("X2");
+
+            SPField.Text = E.Processor.SP.ToString("X4");
+            PCField.Text = E.Processor.PC.ToString("X4");
+
+            FLAGSField.Text = E.Processor.ZeroFlag.ToString() + " " +
+                                E.Processor.SubtractFlag.ToString() + " " +
+                                E.Processor.HalfCarryFlag.ToString() + " " +
+                                E.Processor.CarryFlag.ToString();
+
+            OPField.Text = E.NextOPcode.ToString("X2") + "  " + E.Disassembler.DisassembleMemoryAddress(ref E.Memory, E.Processor.PC); ;
+            IEField.Text = E.Processor.IME ? "Yes" : "No";
+
+            if (E.Processor.CPUHalt) CPUStatField.Text = "Halted";
+            else if (E.Processor.CPUStop) CPUStatField.Text = "Stopped";
+            else CPUStatField.Text = "Ok";
+
+
+        }
+    }
+}
---
+++ trunk/sharpGB/Forms/RomHeaderForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sharpGB
+{
+    public partial class RomHeaderForm : Form
+    {
+        public RomHeaderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RomHeaderForm_Shown(object sender, EventArgs e)
+        {
+            this.ShowHeaderInfo();
+        }
+
+
+        private void ShowHeaderInfo()
+        {
+            // if no rom is loaded, then there is no header data
+            if (!Program.Emulator.ROM.RomLoaded) return;
+
+
+            // gather and display all rom data
+            this.FileSizeField.Text = (Program.Emulator.ROM.RomSize / 1024).ToString() + "kB";
+
+            this.CodeStartField.Text =  Program.Emulator.ROM.Header.BeginCodeExecutionPoint[0].ToString("X2") + " " +
+                                        Program.Emulator.ROM.Header.BeginCodeExecutionPoint[1].ToString("X2") + " " +
+                                        Program.Emulator.ROM.Header.BeginCodeExecutionPoint[2].ToString("X2") + " " +
+                                        Program.Emulator.ROM.Header.BeginCodeExecutionPoint[3].ToString("X2");
+
+            this.GameTitleField.Text = System.Text.Encoding.UTF8.GetString(Program.Emulator.ROM.Header.GameTitle);
+            this.ColorGBField.Text = Program.Emulator.ROM.Header.ColorGB.ToString("X2");
+            this.CartridgeTypeField.Text = Program.Emulator.ROM.Header.CartridgeType.ToString("X2");
+            this.LicenseeHighField.Text = Program.Emulator.ROM.Header.LicenseeHigh.ToString("X2");
+            this.LicenseeLowField.Text = Program.Emulator.ROM.Header.LicenseeLow.ToString("X2");
+            this.SGBIndicatorField.Text = Program.Emulator.ROM.Header.GBFunctionsIndicator.ToString("X2");
+            this.ROMSizeField.Text = Program.Emulator.ROM.Header.ROMSize.ToString("X2");
+            this.RAMSizeField.Text = Program.Emulator.ROM.Header.RAMSize.ToString("X2");
+            this.DestinationCodeField.Text = Program.Emulator.ROM.Header.DestinationCode.ToString("X2");
+            this.LicenseeCodeField.Text = Program.Emulator.ROM.Header.LicenseeCode.ToString("X2");
+            this.ROMVersionField.Text = Program.Emulator.ROM.Header.ROMVersionNumber.ToString("X2");
+            this.ComplementCheckField.Text = Program.Emulator.ROM.Header.ComplementCheck.ToString("X2");
+
+            this.ChecksumField.Text = Program.Emulator.ROM.Header.Checksum[0].ToString("X2") +
+                                        Program.Emulator.ROM.Header.Checksum[1].ToString("X2");
+        }
+
+        private void ColorGBLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
