feee3e10fa2347ce8fce027362390a8dae18c46d
wadim.kehl <wadim.kehl@f0eada0c-1a61-11de-a958-c9a343b5b891>
2009-03-27 15:20:02 +0000

    Breakpoints and included CPU info into main window

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -35,13 +35,12 @@
             this.rOMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.headerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.romHexViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.cPUViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.EmulateNextStepButton = new System.Windows.Forms.Button();
-            this.RunButton = new System.Windows.Forms.Button();
-            this.StopButton = new System.Windows.Forms.Button();
+            this.RunStepsButton = new System.Windows.Forms.Button();
             this.VideoBox = new System.Windows.Forms.PictureBox();
             this.RunBox = new System.Windows.Forms.TextBox();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.InfoBox = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.VideoBox)).BeginInit();
             this.SuspendLayout();
@@ -53,7 +52,7 @@
             this.rOMToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(479, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(755, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -90,8 +89,7 @@
             // 
             this.rOMToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.headerToolStripMenuItem,
-            this.romHexViewToolStripMenuItem,
-            this.cPUViewToolStripMenuItem});
+            this.romHexViewToolStripMenuItem});
             this.rOMToolStripMenuItem.Name = "rOMToolStripMenuItem";
             this.rOMToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
             this.rOMToolStripMenuItem.Text = "Debug";
@@ -110,48 +108,21 @@
             this.romHexViewToolStripMenuItem.Text = "ROM Hex View";
             this.romHexViewToolStripMenuItem.Click += new System.EventHandler(this.romHexViewToolStripMenuItem_Click);
             // 
-            // cPUViewToolStripMenuItem
-            // 
-            this.cPUViewToolStripMenuItem.Name = "cPUViewToolStripMenuItem";
-            this.cPUViewToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.cPUViewToolStripMenuItem.Text = "CPU View";
-            this.cPUViewToolStripMenuItem.Click += new System.EventHandler(this.cPUViewToolStripMenuItem_Click);
-            // 
             // openFileDialog1
             // 
             this.openFileDialog1.Filter = "Gameboy ROMS|*.gb; *.gbc";
             this.openFileDialog1.InitialDirectory = "/";
             this.openFileDialog1.Title = "Open ROM file";
             // 
-            // EmulateNextStepButton
+            // RunStepsButton
             // 
-            this.EmulateNextStepButton.Location = new System.Drawing.Point(274, 27);
-            this.EmulateNextStepButton.Name = "EmulateNextStepButton";
-            this.EmulateNextStepButton.Size = new System.Drawing.Size(183, 23);
-            this.EmulateNextStepButton.TabIndex = 6;
-            this.EmulateNextStepButton.Text = "Emulate Next Step";
-            this.EmulateNextStepButton.UseVisualStyleBackColor = true;
-            this.EmulateNextStepButton.Click += new System.EventHandler(this.EmulateNextStepButton_Click);
-            // 
-            // RunButton
-            // 
-            this.RunButton.Location = new System.Drawing.Point(274, 56);
-            this.RunButton.Name = "RunButton";
-            this.RunButton.Size = new System.Drawing.Size(40, 23);
-            this.RunButton.TabIndex = 7;
-            this.RunButton.Text = "Run:";
-            this.RunButton.UseVisualStyleBackColor = true;
-            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
-            // 
-            // StopButton
-            // 
-            this.StopButton.Location = new System.Drawing.Point(373, 56);
-            this.StopButton.Name = "StopButton";
-            this.StopButton.Size = new System.Drawing.Size(84, 23);
-            this.StopButton.TabIndex = 8;
-            this.StopButton.Text = "Stop!";
-            this.StopButton.UseVisualStyleBackColor = true;
-            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            this.RunStepsButton.Location = new System.Drawing.Point(343, 28);
+            this.RunStepsButton.Name = "RunStepsButton";
+            this.RunStepsButton.Size = new System.Drawing.Size(88, 23);
+            this.RunStepsButton.TabIndex = 7;
+            this.RunStepsButton.Text = "Run Steps:";
+            this.RunStepsButton.UseVisualStyleBackColor = true;
+            this.RunStepsButton.Click += new System.EventHandler(this.RunStepsButton_Click);
             // 
             // VideoBox
             // 
@@ -167,22 +138,41 @@
             // 
             // RunBox
             // 
-            this.RunBox.Location = new System.Drawing.Point(320, 57);
+            this.RunBox.Location = new System.Drawing.Point(437, 30);
             this.RunBox.Name = "RunBox";
-            this.RunBox.Size = new System.Drawing.Size(42, 20);
+            this.RunBox.Size = new System.Drawing.Size(75, 20);
             this.RunBox.TabIndex = 10;
             this.RunBox.Text = "1";
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(274, 29);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(63, 23);
+            this.RunButton.TabIndex = 11;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            // 
+            // InfoBox
+            // 
+            this.InfoBox.Location = new System.Drawing.Point(278, 58);
+            this.InfoBox.Multiline = true;
+            this.InfoBox.Name = "InfoBox";
+            this.InfoBox.ReadOnly = true;
+            this.InfoBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InfoBox.Size = new System.Drawing.Size(234, 217);
+            this.InfoBox.TabIndex = 12;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(479, 288);
+            this.ClientSize = new System.Drawing.Size(755, 287);
+            this.Controls.Add(this.InfoBox);
+            this.Controls.Add(this.RunButton);
             this.Controls.Add(this.RunBox);
             this.Controls.Add(this.VideoBox);
-            this.Controls.Add(this.StopButton);
-            this.Controls.Add(this.RunButton);
-            this.Controls.Add(this.EmulateNextStepButton);
+            this.Controls.Add(this.RunStepsButton);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -210,12 +200,11 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem romHexViewToolStripMenuItem;
-        private System.Windows.Forms.Button EmulateNextStepButton;
-        private System.Windows.Forms.ToolStripMenuItem cPUViewToolStripMenuItem;
-        private System.Windows.Forms.Button RunButton;
-        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button RunStepsButton;
         private System.Windows.Forms.PictureBox VideoBox;
         private System.Windows.Forms.TextBox RunBox;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.TextBox InfoBox;
     }
 }
 
